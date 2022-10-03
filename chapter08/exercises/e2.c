d10a546368b761ec1394c36f0c13720772f14dd5
ryantoddgarza <ryantoddgarza@gmail.com>
2022-10-03 18:39:41 -0500

    feat: add chapter 8 exercise 2

---
+++ chapter08/exercises/e2.c
@@ -0,0 +1,3 @@
+// To use a digit character as an array subscript, we would need to scale the
+// underlying character codes by writing `arr[d - '0']` where `d` is a digit
+// character and the underlying character codes are consecutive.
