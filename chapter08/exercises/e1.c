c9532ae72c3c25624ac5ece0dca8215c3651c1de
ryantoddgarza <ryantoddgarza@gmail.com>
2022-10-03 18:29:51 -0500

    feat: add chapter 8 exercise 1

---
+++ chapter08/exercises/e1.c
@@ -0,0 +1,3 @@
+// Using the expression `sizeof(a) / sizeof(t)` where `t` is the type of `a`'s
+// elements is problematic because it is not possible to check for code
+// correctness without locating the declaration of `a`.
