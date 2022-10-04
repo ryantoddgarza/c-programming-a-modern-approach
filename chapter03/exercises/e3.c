5aaab3666cbe292a33416bd6d662f1c2fcb2ee59
ryantoddgarza <ryantoddgarza@gmail.com>
2022-10-03 23:48:46 -0500

    chore: convert prose answers to text files

--- chapter03/exercises/e3.c
+++
@@ -1,9 +0,0 @@
-// (a) Strings `%d` and ` %d` are equivalent.
-
-// (b) Strings `%d-%d-%d` and `%d -%d -%d` are equivalent.
-
-// (c) Strings `%f` and `%f ` are NOT equivalent. `scanf` reads white-space
-// characters from the input until it reaches a non-white-space character. A
-// space at the end of a format string causes the program to hang.
-
-// (d) Strings `%f,%f` and `%f, %f` are equivalent.
--- chapter08/exercises/e2.c
+++
@@ -1,3 +0,0 @@
-// To use a digit character as an array subscript, we would need to scale the
-// underlying character codes by writing `arr[d - '0']` where `d` is a digit
-// character and the underlying character codes are consecutive.
